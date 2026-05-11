c839670c7df9fe86e0b84bd75977435c4712baa8
joanamaia03 <93883152+joanamaia03@users.noreply.github.com>
2026-05-11 15:48:16 +0100

    changed ARScreen

--- ARScreen.tsx
+++ ARScreen.tsx
@@ -11,7 +11,7 @@ import {
 import { WebView } from 'react-native-webview';
 
 
-const WEB_AR_URL = 'https://hiukim.github.io/mind-ar-js-doc/samples/basic.html';
+const WEB_AR_URL = 'https://github.com/joanamaia03/TESE-NutlyAR/tree/web-ar';
 
 export default function ARScreen() {
   const [cameraGranted, setCameraGranted] = React.useState<boolean | null>(null);
@@ -76,6 +76,7 @@ export default function ARScreen() {
         mixedContentMode="always"
         originWhitelist={['*']}
         setSupportMultipleWindows={false}
+        onPermissionRequest={(event: { grant: (arg0: any) => any; resources: any; }) => event.grant(event.resources)}
       />
 
       <View style={styles.overlay}>
